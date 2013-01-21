a7376af13ec2948d88bbd2de42f4f2323e161532
dredei <xkadidas@gmail.com>
2013-01-21 20:03:07 +0200

    Фикс скрипта обновления.

--- script/classes/version.class.php
+++ script/classes/version.class.php
@@ -1,7 +1,12 @@
 <?php
 class version {
 	function getVersion() {
-		$version = 101.01;
+		global $config;
+		$db = new db_e;
+		
+		$query = "SELECT scriptVersion FROM ".$config['db_prefix']."external_settings";
+		$res = $db->ExecQuery( $query );
+		$version = $res['rows'][0]['scriptVersion'];		
 		return $version;
 	}
 }
