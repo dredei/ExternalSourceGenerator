83c92ba363a250a64802b2fabcddc42b46bb3d88
dredei <xkadidas@gmail.com>
2013-02-03 18:21:11 +0200

    Обновлена авторизация. Обновлены файлы для движков.

--- script/auth.php
+++ script/auth.php
@@ -4,8 +4,11 @@ require_once 'config.php';
 require_once 'classes/settings.class.php';
 
 $st = new settings;
-
-$token = htmlspecialchars( $_COOKIE['token'] );	
+if ( isset($_COOKIE['token']) ) {
+	$token = htmlspecialchars( $_COOKIE['token'] );
+} else {
+	$token = 'pfjkdllfmdk';
+}
 if ( $st->checkToken( $token ) === false ) {
 	setcookie('token', '');
 	$_SESSION['lg'] = 'nlg';
