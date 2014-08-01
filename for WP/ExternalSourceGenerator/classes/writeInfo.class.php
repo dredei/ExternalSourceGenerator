f37704491aad69e311ed7f282c0af5263615eaf9
dredei <xkadidas@gmail.com>
2014-08-01 11:39:56 +0300

    fix

--- for WP/ExternalSourceGenerator/classes/writeInfo.class.php
+++ for WP/ExternalSourceGenerator/classes/writeInfo.class.php
@@ -10,7 +10,7 @@ class WriteInfo {
 		$browser->Browser( $userAgent );
 		$browser_name = $browser->getBrowser();
 		$browser_version = $browser->getVersion();
-		if ( ( $browser_name == 'unknown' ) or ( $browser->isRobot() ) )
+		if ( ( $browser_name == 'unknown' ) || ( $browser->isRobot() ) )
 			return;
 		$os = $browser->getPlatform();
 		$ua = $browser->getUserAgent();
